36b77650ef9b15e60a33006d934da530fd0a9ce2
webapis <60083568+webapis@users.noreply.github.com>
2022-02-18 10:03:59 +0500

    D

--- inputConfig.js
+++ inputConfig.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 const Apify = require('apify');
 
 module.exports= async()=>{
-    if (process.env.CLOUD){
-
+    if (process.env.CLOUD==='TRUE'){
+console.log('cloud env is set')
         const {startUrl,JSONfileName,marka,gender,category,subcategory}=await Apify.getInput();
         process.env.startUrl=startUrl
         process.env.JSONfileName=JSONfileName
@@ -11,5 +11,7 @@ module.exports= async()=>{
         process.env.gender=gender
         process.env.category=category
         process.env.subcategory=subcategory
-    } 
+    } else{
+        console.log('cloud env is NOT set') 
+    }
 }
