50171675e58cc8521248bb87d4a182c3457247b0
webapis <60083568+webapis@users.noreply.github.com>
2022-03-14 10:22:42 +0500

    cvcvc

--- main.js
+++ main.js
@@ -116,7 +116,7 @@ Apify.main(async () => {
         await crawler.run();
         const ds= await dataset.getData()
         console.log('items......',ds.items)
-
+        console.log('ds......',ds)
      //   fs.writeFileSync(`${JSONfileName}.json`, JSON.stringify(ds.items))
         //const upload = await cloudinary.v2.uploader.upload(`${JSONfileName}.json`, { public_id: JSONfileName, resource_type: "auto", invalidate: true })
         await uploadToAtlas({ data: ds.items })
