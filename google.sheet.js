54ab36eb32bfa1954f03d0ec46fd4e9fca4b5600
webapis <60083568+webapis@users.noreply.github.com>
2022-03-25 08:20:42 +0500

    google auth

--- google.sheet.js
+++ google.sheet.js
@@ -1,20 +1,27 @@
 const { nodeFetch } = require('./node-fetch')
-async function getSheetValues({ access_token, spreadsheetId, range,refresh_token }) {
+async function getSheetValues({ access_token, spreadsheetId, range, refresh_token }) {
 
-    const response = await nodeFetch({ host: 'sheets.googleapis.com', path: `/v4/spreadsheets/${spreadsheetId}/values/${range}`, method: 'get', headers: { 'User-Agent': 'node.js', 'Content-Type': 'application/json', 'Authorization': `Bearer ${access_token}` } })
+  const sheetresponse = await nodeFetch({ host: 'sheets.googleapis.com', path: `/v4/spreadsheets/${spreadsheetId}/values/${range}`, method: 'get', headers: { 'User-Agent': 'node.js', 'Content-Type': 'application/json', 'Authorization': `Bearer ${access_token}` } })
 
+  debugger;
+
+  let data = JSON.parse(sheetresponse)
+  const error = data['error']
+  if (error) {
     debugger;
+    //refresh token
+    const authresponse = await nodeFetch({ host: 'workflow-runner.netlify.app', path: `/.netlify/functions/google-refresh?refresh_token=${refresh_token}`, method: 'get', headers: { 'User-Agent': 'node.js', 'Content-Type': 'application/json' } })
+    let authData = JSON.parse(authresponse)
 
-    let data = JSON.parse(response)
-    const error = data['error']
-    if (error) {
-        debugger;
-        //refresh token
-       const response = await nodeFetch({ host: 'workflow-runner.netlify.app', path: `/.netlify/functions/google-refresh?refresh_token=${refresh_token}`, method: 'get', headers: { 'User-Agent': 'node.js', 'Content-Type': 'application/json' } }) 
-       data = JSON.parse(response)
-   }
+    const sheetresponse = await nodeFetch({ host: 'sheets.googleapis.com', path: `/v4/spreadsheets/${spreadsheetId}/values/${range}`, method: 'get', headers: { 'User-Agent': 'node.js', 'Content-Type': 'application/json', 'Authorization': `Bearer ${authData.access_token}` } })
+    return JSON.parse(sheetresponse)
+
+  } else {
     debugger;
-    console.log('token data',data)
+    console.log('sheet data', data)
     return data
+
+  }
+
 }
 module.exports = { getSheetValues }
