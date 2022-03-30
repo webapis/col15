dd0b49b19ab598e34fe1daf1e25b2e0b8e6e0273
webapis <60083568+webapis@users.noreply.github.com>
2022-03-30 14:40:10 +0500

    v

--- google.oauth.js
+++ google.oauth.js
@@ -2,12 +2,13 @@ const { fbRest } = require('../firebase-rest')
 const fetch = require('node-fetch')
 const fbDatabase = fbRest()
 async function getGoogleToken() {
-debugger;
+  debugger;
   const selectedWorkspace = process.env.selectedWorkspace
 
-  const {inc:currenttimestamp} = await fbDatabase.ref("/").update({ inc: { ".sv": "timestamp" } })
-debugger;
-  if ((parseInt( process.env.google_expires_in) * 1000 +parseInt( process.env.google_timestamp)) > currenttimestamp) {
+  const { inc: currenttimestamp } = await fbDatabase.ref("/").update({ inc: { ".sv": "timestamp" } })
+  const issuetime = parseInt(process.env.google_expires_in) * 1000 + parseInt(process.env.google_timestamp)
+  debugger;
+  if (issuetime > currenttimestamp) {
     debugger;
     //refresh token
     const fetchpath = `https://workflow-runner.netlify.app/.netlify/functions/google-refresh?refresh_token=${process.env.google_refresh_token}`
