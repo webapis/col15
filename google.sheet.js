2beb6ced16bd3021506953956ea6e2af2adc81a2
webapis <60083568+webapis@users.noreply.github.com>
2022-03-30 14:31:58 +0500

    v

--- google.sheet.js
+++ google.sheet.js
@@ -2,9 +2,9 @@ const { nodeFetch } = require('./node-fetch')
 async function getSheetValues({ access_token, spreadsheetId, range }) {
 
   const sheetresponse = await nodeFetch({ host: 'sheets.googleapis.com', path: `/v4/spreadsheets/${spreadsheetId}/values/${range}`, method: 'get', headers: { 'User-Agent': 'node.js', 'Content-Type': 'application/json', 'Authorization': `Bearer ${access_token}` } })
-
+debugger;
   let data = JSON.parse(sheetresponse)
-
+debugger;
   return data
 
 }
