73634dd83b5ae0c7fe1e1820f384f174d7c1aed4
webapis <60083568+webapis@users.noreply.github.com>
2022-04-27 11:34:03 +0500

    Update google.oauth.js

--- google/google.oauth.js
+++ google/google.oauth.js
@@ -2,20 +2,46 @@ require('dotenv').config()
 
 const { refreshToken } = require('./google-refresh')
 async function getGoogleToken() {
-  
+
+  if (process.env.access_token) {
+    const issuetime = parseInt(process.env.expires_in) * 1000 + parseInt(process.env.lasttimestamp)
+    const currenttimestamp = Date.now()
+    if (issuetime < currenttimestamp) {
+      return process.env.access_token
+    } else {
+      const authresponse = await refreshToken(process.env.GOOGLE_REFRESH_TOKEN)
+      let authData = JSON.parse(authresponse)
+      const {
+        access_token,
+        expires_in } = authData
+      process.env.expires_in = expires_in
+      process.env.access_token = access_token
+      process.env.lasttimestamp = Date.now()
+
+      return access_token
+
+    }
+
+
+  } else {
 
     const authresponse = await refreshToken(process.env.GOOGLE_REFRESH_TOKEN)
-
-
     let authData = JSON.parse(authresponse)
     const {
-      access_token } = authData
+      access_token,
+      expires_in } = authData
+    process.env.expires_in = expires_in
+    process.env.access_token = access_token
+    process.env.lasttimestamp = Date.now()
 
-      console.log('authData',authData)
     return access_token
-    //update firebase
+  }
 
-  
+
+
+
+
+
 }
 
 module.exports = { getGoogleToken }
