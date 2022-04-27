423c70b9c8c699af348c20b42723199171776527
webapis <60083568+webapis@users.noreply.github.com>
2022-04-27 09:15:00 +0500

    Update google-refresh.js

--- google/google-refresh.js
+++ google/google-refresh.js
@@ -7,7 +7,7 @@ console.log('CLIENT_SECRET', CLIENT_SECRET)
 
 
 async function refreshToken(refresh_token) {
-
+    console.log('refresh_token...44444',refresh_token)
     const response = await nodeFetch({ host: 'oauth2.googleapis.com', path: `/token?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=refresh_token&refresh_token=${refresh_token}`, method: 'post', headers: { 'User-Agent': 'node.js', 'Content-Type': 'application/json' } })
     debugger;
     return response
